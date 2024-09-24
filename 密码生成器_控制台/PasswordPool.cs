13411969e579d573fc1b2e813052346cbd0e9ad1
suyangzuo <suyangzuo@163.com>
2024-09-24 14:10:07 +0800

    初始提交

---
+++ 密码生成器_控制台/PasswordPool.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 密码生成器_控制台
+{
+    internal static class PasswordGenerator
+    {
+        public static readonly char[] CharsSymbol = new char[32];
+        public static readonly char[] CharsDigit = new char[10];
+        public static readonly char[] CharsLetter = new char[52];
+
+        public static bool LetterChecked { get; set; } = true;
+        public static bool DigitChecked { get; set; } = true;
+        public static bool SymbolChecked { get; set; } = true;
+
+        [Flags]
+        public enum PasswordTypeEnum
+        {
+            Letter = 1,
+            Digit = 2,
+            Symbol = 4,
+            LetterAndDigit = Letter | Digit,
+            LetterAndSymbol = Letter | Symbol,
+            All = Letter | Digit | Symbol
+        }
+
+        public static PasswordTypeEnum PasswordType { get; set; } = PasswordTypeEnum.All;
+
+        private static void InitChars()
+        {
+            for (var i = 0; i < CharsDigit.Length; i++)
+            {
+                CharsDigit[i] = (char)(i + 48);
+            }
+
+            for (var i = 0; i < CharsLetter.Length; i++)
+            {
+                CharsLetter[i] = (char)(i + (i <= 25 ? 65 : 71));
+            }
+
+            for (var i = 0; i < CharsSymbol.Length; i++)
+            {
+                CharsSymbol[i] = (char)(i + i switch
+                {
+                    < 15 => 33,
+                    < 22 => 43,
+                    < 28 => 69,
+                    _ => 95
+                });
+            }
+        }
+
+        private static char[] GetPasswordCharPool()
+        {
+            InitChars();
+            char[] pool;
+            switch (PasswordType)
+            {
+                case PasswordTypeEnum.Letter:
+                    pool = new char[CharsLetter.Length];
+                    for (var i = 0; i < pool.Length; i++)
+                    {
+                        pool[i] = CharsLetter[i];
+                    }
+
+                    break;
+                case PasswordTypeEnum.LetterAndDigit:
+                    pool = new char[CharsLetter.Length + CharsDigit.Length];
+                    for (var i = 0; i < pool.Length; i++)
+                    {
+                        pool[i] = i < CharsLetter.Length ? CharsLetter[i] : CharsDigit[i - CharsLetter.Length];
+                    }
+
+                    break;
+                case PasswordTypeEnum.LetterAndSymbol:
+                    pool = new char[CharsLetter.Length + CharsSymbol.Length];
+                    for (var i = 0; i < pool.Length; i++)
+                    {
+                        pool[i] = i < CharsLetter.Length ? CharsLetter[i] : CharsSymbol[i - CharsLetter.Length];
+                    }
+
+                    break;
+                case PasswordTypeEnum.All:
+                case PasswordTypeEnum.Digit:
+                case PasswordTypeEnum.Symbol:
+                default:
+                    pool = new char[CharsLetter.Length + CharsDigit.Length + CharsSymbol.Length];
+                    for (var i = 0; i < pool.Length; i++)
+                    {
+                        if (i < CharsLetter.Length)
+                        {
+                            pool[i] = CharsLetter[i];
+                        }
+                        else if (i - CharsLetter.Length < CharsDigit.Length)
+                        {
+                            pool[i] = CharsDigit[i - CharsLetter.Length];
+                        }
+                        else
+                        {
+                            pool[i] = CharsSymbol[i - CharsLetter.Length - CharsDigit.Length];
+                        }
+                    }
+
+                    break;
+            }
+
+            return pool;
+        }
+
+        public static char[] GetPassword(int length)
+        {
+            var r = new Random(Guid.NewGuid().GetHashCode());
+            var passwordCharPool = GetPasswordCharPool();
+            var password = new char[length];
+
+            var letterFixedIndex = r.Next(0, length);
+            int digitFixedIndex = -1;
+            int symbolFixedIndex = -1;
+
+            switch (PasswordType)
+            {
+                case PasswordTypeEnum.LetterAndDigit:
+                {
+                    do
+                    {
+                        digitFixedIndex = r.Next(0, length);
+                    } while (digitFixedIndex == letterFixedIndex);
+                    
+                    break;
+                }
+                case PasswordTypeEnum.LetterAndSymbol:
+                {
+                    do
+                    {
+                        symbolFixedIndex = r.Next(0, length);
+                    } while (symbolFixedIndex == letterFixedIndex);
+                    
+                    break;
+                }
+                case PasswordTypeEnum.Letter:
+                case PasswordTypeEnum.Digit:
+                case PasswordTypeEnum.Symbol:
+                case PasswordTypeEnum.All:
+                default:
+                {
+                    do
+                    {
+                        digitFixedIndex = r.Next(0, length);
+                    } while (digitFixedIndex == letterFixedIndex);
+
+                    do
+                    {
+                        symbolFixedIndex = r.Next(0, length);
+                    } while (symbolFixedIndex == digitFixedIndex || symbolFixedIndex == letterFixedIndex);
+                    
+                    break;
+                }
+            }
+
+            for (var i = 0; i < length; i++)
+            {
+                switch (PasswordType)
+                {
+                    case PasswordTypeEnum.Letter:
+                        password[i] = CharsLetter[r.Next(0, CharsLetter.Length)];
+                        break;
+                    case PasswordTypeEnum.LetterAndDigit:
+                    {
+                        if (i == letterFixedIndex)
+                        {
+                            password[i] = CharsLetter[r.Next(0, CharsLetter.Length)];
+                        }
+                        else if (i == digitFixedIndex)
+                        {
+                            password[i] = CharsDigit[r.Next(0, CharsDigit.Length)];
+                        }
+                        else
+                        {
+                            password[i] = passwordCharPool[r.Next(0, passwordCharPool.Length)];
+                        }
+                        
+                        break;
+                    }
+                    case PasswordTypeEnum.LetterAndSymbol:
+                    {
+                        if (i == letterFixedIndex)
+                        {
+                            password[i] = CharsLetter[r.Next(0, CharsLetter.Length)];
+                        }
+                        else if (i == symbolFixedIndex)
+                        {
+                            password[i] = CharsSymbol[r.Next(0, CharsSymbol.Length)];
+                        }
+                        else
+                        {
+                            password[i] = passwordCharPool[r.Next(0, passwordCharPool.Length)];
+                        }
+
+                        break;
+                    }
+                    case PasswordTypeEnum.Digit:
+                    case PasswordTypeEnum.Symbol:
+                    case PasswordTypeEnum.All:
+                    default:
+                    {
+                        if (i == letterFixedIndex)
+                        {
+                            password[i] = CharsLetter[r.Next(0, CharsLetter.Length)];
+                        }
+                        else if (i == digitFixedIndex)
+                        {
+                            password[i] = CharsDigit[r.Next(0, CharsDigit.Length)];
+                        }
+                        else if (i == symbolFixedIndex)
+                        {
+                            password[i] = CharsSymbol[r.Next(0, CharsSymbol.Length)];
+                        }
+                        else
+                        {
+                            password[i] = passwordCharPool[r.Next(0, passwordCharPool.Length)];
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            return password;
+        }
+    }
+}
---
+++ 密码生成器_控制台/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace 密码生成器_控制台;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            var chooseHash = new HashSet<string?> { "1", "2", "3", "4" };
+            string? choose;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("选择密码类型：");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("1. 字母\n2. 字母 + 数字\n3. 字母 + 符号\n4. 字母 + 数字 + 符号\n");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write("输入序号并回车：");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                choose = Console.ReadLine();
+                if (!string.IsNullOrEmpty(choose) && chooseHash.Contains(choose)) continue;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("选择错误，请重选！\n");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            } while (string.IsNullOrEmpty(choose) || !chooseHash.Contains(choose));
+
+            var number = int.Parse(choose);
+
+            PasswordGenerator.PasswordType = number switch
+            {
+                1 => PasswordGenerator.PasswordTypeEnum.Letter,
+                2 => PasswordGenerator.PasswordTypeEnum.LetterAndDigit,
+                3 => PasswordGenerator.PasswordTypeEnum.LetterAndSymbol,
+                4 => PasswordGenerator.PasswordTypeEnum.All,
+                _ => throw new Exception()
+            };
+
+            int length;
+            var lengthOk = false;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write("输入密码长度：");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                lengthOk = int.TryParse(Console.ReadLine(), out length);
+                if (!lengthOk)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("输入错误，请重新输入！\n");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else if (length < 6)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("密码长度至少为6，请重新输入！\n");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            } while (!lengthOk || length < 6);
+
+            var password = PasswordGenerator.GetPassword(length);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("已成功生成密码：");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            foreach (var item in password)
+            {
+                if (PasswordGenerator.CharsLetter.Contains(item))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else if (PasswordGenerator.CharsDigit.Contains(item))
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+
+                Console.Write(item);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\n\n-----------------------------\n");
+        }
+    }
+}
